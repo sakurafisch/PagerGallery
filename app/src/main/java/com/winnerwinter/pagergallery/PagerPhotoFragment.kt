f31fd676421e484bae2347ee5dfbc2bde3985001
sakurafisch <winnerwinter1998@gmail.com>
2020-12-31 13:48:48 +0800

    添加保存图片功能

--- app/src/main/java/com/winnerwinter/pagergallery/PagerPhotoFragment.kt
+++ app/src/main/java/com/winnerwinter/pagergallery/PagerPhotoFragment.kt
@@ -1,17 +1,37 @@
 package com.winnerwinter.pagergallery
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.fragment_pager_photo.*
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.pager_photo_view.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+const val REQUEST_WRITE_EXTERNAL_STORAGE = 1
 
 /**
  * A simple [Fragment] subclass.
@@ -32,8 +52,8 @@ class PagerPhotoFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pager_photo, container, false)
@@ -54,7 +74,81 @@ class PagerPhotoFragment : Fragment() {
             }
         })
 
-        viewPager2.setCurrentItem(arguments?.getInt("PHOTO_POSITION")?:0, false)
+        viewPager2.setCurrentItem(arguments?.getInt("PHOTO_POSITION") ?: 0, false)
+        saveButtonImageView.setOnClickListener {
+            if (Build.VERSION.SDK_INT < 29) {
+                if (
+                        ContextCompat.checkSelfPermission(
+                                requireContext(),
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermissions(
+                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                            REQUEST_WRITE_EXTERNAL_STORAGE)
+                }
+            } else {
+                viewLifecycleOwner.lifecycleScope.launch {
+                    savePhoto()
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_WRITE_EXTERNAL_STORAGE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        savePhoto()
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private suspend fun savePhoto() {
+        withContext(Dispatchers.IO) {
+            val holder =
+                    (viewPager2[0] as RecyclerView).findViewHolderForAdapterPosition(viewPager2.currentItem)
+                            as PagerPhotoViewHolder
+            val bitmap = holder.itemView.pagePhoto.drawable.toBitmap()
+            if (Build.VERSION.SDK_INT < 29) {
+                if (null == MediaStore.Images.Media.insertImage(requireContext().contentResolver, bitmap, "", "")) {
+                    Toast.makeText(requireContext(),"存储失败", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                val saveUri = requireContext().contentResolver.insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        ContentValues()
+                )?: kotlin.run {
+                    MainScope().launch {
+                        Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+                    }
+                    return@withContext
+                }
+                requireContext().contentResolver.openOutputStream(saveUri).use {
+                    if (bitmap.compress(Bitmap.CompressFormat.JPEG, 90, it)) {
+                        MainScope().launch {
+                            Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        MainScope().launch {
+                            Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
     }
 
     companion object {
@@ -69,11 +163,11 @@ class PagerPhotoFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PagerPhotoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                PagerPhotoFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
